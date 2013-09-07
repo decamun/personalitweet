d110a151239ebac6a6a9e9f70dc328aabc778583
xnieamo <xiaomao_ding@yahoo.com>
2013-09-07 09:01:53 -0400

    minor GUI

--- PersonaliTweet/src/Board.java
+++ PersonaliTweet/src/Board.java
@@ -1,38 +1,82 @@
+
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JFrame;
 import java.util.*;
-import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.*;
 import javax.swing.JTextField;
+import javax.swing.event.MouseInputListener;
 
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Decamun
  */
-public class Board extends JPanel {
+public class Board extends JPanel implements MouseListener, ActionListener{
+
     ArrayList<Sprite> objects;
     JTextField tf;
+    JButton bt;
+    String text;
+
     public Board(ArrayList<Sprite> objects) {
         this.objects = objects;
         tf = new JTextField(30);
+        bt = new JButton("Click");
+        bt.addActionListener(this);
         add(tf);
+        add(bt);
         
     }
-    public String getText()
-    {
+
+    public String getText() {
         return tf.getText();
     }
+
     @Override
     public void paintComponent(Graphics g) {
-       // g.drawRect(100, 100, 100, 100);
-//        for(Sprite object : objects) {
-//            object.paint(g);
+        g.setColor(Color.blue);
+        g.fillRect(0, 0, 1000, 1000);
 //        }
     }
-  
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String event = e.getActionCommand();
+        if(event.equals("click"));
+            
+    }
 }
